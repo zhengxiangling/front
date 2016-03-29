70169136dddfcf5e389f686f48ceb956230b67d0
zhengxiangling <1807588108@qq.com>
2016-03-29 19:25:12 +0800

    change js

--- term-i/home page/js/index.js
+++ term-i/home page/js/index.js
@@ -1,3 +1,6 @@
 $(function(){
-	$(".zimu").animate({"margin-top":"313px"},{duration: 1800, easing: "easeOutBounce"});
+	function slider(){
+		$(".zimu").animate({"margin-top":"313px"},{duration: 3000, easing: "easeOutBounce"});
+	}
+	window.onload=slider;
 })
