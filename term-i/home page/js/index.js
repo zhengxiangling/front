fd5552e1d3885acb4333e8f8fb12ac1533536fdf
zhengxiangling <1807588108@qq.com>
2016-03-29 19:36:03 +0800

    修改交互效果

--- term-i/home page/js/index.js
+++ term-i/home page/js/index.js
@@ -3,4 +3,13 @@ $(function(){
 		$(".zimu").animate({"margin-top":"313px"},{duration: 3000, easing: "easeOutBounce"});
 	}
 	window.onload=slider;
+	$(".buttom2").hide();
+	$(".buttom1").mouseenter(function(){
+		$(".buttom2").fadeIn(500);
+		$(".buttom1").hide();
+	});
+	$(".buttom2").mouseout(function(){
+		$(".buttom1").fadeIn(500);
+		$(".buttom2").hide();
+	});
 })
