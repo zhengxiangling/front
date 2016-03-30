80a96b26581ed5b2ab1e88d5e79c4afe2ecbb0b4
zhengxiangling <1807588108@qq.com>
2016-03-30 12:45:06 +0800

    二级页面

---
+++ term-i/second page/js/index.js
@@ -0,0 +1,14 @@
+$(function(){
+/*	$(".buttom2").hide();
+	$(".buttom1").mouseenter(function(){
+		$(".buttom2").fadeIn(500);
+		$(".buttom1").hide();
+	});
+	$(".buttom2").mouseout(function(){
+		$(".buttom1").fadeIn(500);
+		$(".buttom2").hide();
+	});*/
+	var $winWidth=$(window).width();
+	var $winHeight=$(window).height();
+	$(".wrapper").height($winHeight);
+})
